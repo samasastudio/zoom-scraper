69bcc65132cb39e56fc6a646e934b65f46f20a7b
Sam Johnson <samj@iconcollective.com>
2020-04-13 18:54:18 -0700

    csv to json in progress...

--- scripts/run.js
+++ scripts/run.js
@@ -1,31 +1,111 @@
-//import the week's office hour data
+//**import the week's office hour data**
 fs = require('fs');
-var bang = require("../pastJSON/bang020520");
-var derek = require("../pastJSON/derek020620");
-var jerry = require("../pastJSON/jerry020520");
-var johnvon = require("../pastJSON/johnvon020720");
-var kerry = require("../pastJSON/kerry020620");
-var kyle = require("../pastJSON/kyle020520");
-var malachi = require("../pastJSON/malachi020620");
-var max = require("../pastJSON/max020420");
-var paul = require("../pastJSON/paul020620");
-var petey = require("../pastJSON/petey020520");
-var sam = require("../pastJSON/sam020520");
-var scott = require("../pastJSON/scott020620");
-//create array for processing
-var allMeets = [
-  bang,
-  derek,
-  jerry,
-  johnvon,
-  kerry,
-  malachi,
-  max,
-  paul,
-  petey,
-  sam,
-  scott
+const csv = require('csvtojson');
+// var bang = require("../pastJSON/bang020520");
+// var derek = require("../pastJSON/derek020620");
+// var jerry = require("../pastJSON/jerry020520");
+// var johnvon = require("../pastJSON/johnvon020720");
+var grooveAnalysis = './scripts/reports/grooveAnalysis.csv';
+var meganK = './scripts/reports/meganK.csv';
+var malachiM = './scripts/reports/malachiM.csv';
+var davidM = './scripts/reports/davidM.csv'
+var maxP = './scripts/reports/maxP.csv'
+var nickA = './scripts/reports/nickA.csv'
+var derekP = './scripts/reports/derekP.csv'
+var beatRecipes = './scripts/reports/beatRecipes.csv'
+var jonP = './scripts/reports/jonP.csv'
+var prestonW = './scripts/reports/prestonW.csv'
+var mattB = './scripts/reports/mattB.csv'
+var paulL = './scripts/reports/paulL.csv'
+var kristenGP = './scripts/reports/kristenGP.csv'
+var anthonyP = './scripts/reports/anthonyP.csv'
+var remixLab = './scripts/reports/remixLab.csv'
+var orionN = './scripts/reports/orionN.csv'
+var jerryD = './scripts/reports/jerryD.csv'
+var namasteInside = './scripts/reports/namasteInside.csv'
+var richardN = './scripts/reports/richardN.csv'
+var lyricistL = './scripts/reports/lyricistL.csv'
+var musicHistory = './scripts/reports/musicHistory.csv'
+var peteyE = './scripts/reports/peteyE.csv'
+var samB = './scripts/reports/samB.csv'
+var desM = './scripts/reports/desM.csv'
+var kristianR = './scripts/reports/kristianR.csv'
+var aaronP = './scripts/reports/aaronP.csv'
+var connorS = './scripts/reports/connorS.csv'
+var michaelD = './scripts/reports/michaelD.csv'
+var mattN = './scripts/reports/mattN.csv'
+var daneM = './scripts/reports/daneM.csv'
+var romanP = './scripts/reports/romanP.csv'
+var kyleS = './scripts/reports/kyleS.csv'
+var gregS = './scripts/reports/gregS.csv'
+var soundDesignClub = './scripts/reports/soundDesignClub'
+var taylrR = './scripts/reports/taylrR'
+var ericP = './scripts/reports/ericP'
+var scottZ = './scripts/reports/scottZ'
+
+//**create array for processing**
+var allCSV = [
+  grooveAnalysis,
+  meganK,
+  malachiM,
+  davidM,
+  maxP,
+  nickA,
+  derekP,
+  beatRecipes,
+  jonP,
+  prestonW,
+  mattB,
+  paulL,
+  kristenGP,
+  anthonyP,
+  remixLab,
+  orionN,
+  jerryD,
+  namasteInside,
+  richardN,
+  lyricistL,
+  musicHistory,
+  peteyE,
+  samB,
+  desM,
+  kristianR,
+  aaronP,
+  connorS,
+  michaelD,
+  mattN,
+  daneM,
+  romanP,
+  kyleS,
+  gregS,
+  soundDesignClub,
+  taylrR,
+  ericP,
+  scottZ,
 ]
+//** MAP CSV **/
+var convert = (path) => {
+  csv()
+  .fromFile(path)
+  .then((jsonObj) => {
+    console.log(jsonObj)
+  })
+  var json = await csv().fromFile(path);
+
+  return json;
+}
+
+convert(desM) //?
+
+
+// **IN PROGRESS** */
+// var allFiles = require('require-all')({dirname: __dirname +'/stuff'})
+// allMeets = [];
+
+// for (var meet in allFiles) {
+//   allMeets.push(allFiles[meet])
+// }
+
 //count attendance eliminates duplicates and counts the number of students in meeting
 var countAttendance = function (meeting) {
     meeting.length //?
